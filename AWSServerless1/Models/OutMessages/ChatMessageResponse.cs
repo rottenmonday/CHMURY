fa1cfd04bb7d2ec3d0077772606581038c6d9ef1
Krzysztof Woźniak <krzysztof.wozniak1b@gmail.com>
2020-06-15 02:32:06 +0200

    Added RoomId property to ChatMessageResponse

--- AWSServerless1/Models/OutMessages/ChatMessageResponse.cs
+++ AWSServerless1/Models/OutMessages/ChatMessageResponse.cs
@@ -9,13 +9,20 @@ namespace AWSServerless1.Models.OutMessages
     {
         [JsonPropertyName("messageType")]
         public MessageType MessageType { get; set; } = MessageType.ChatMessageResponse;
+
         [JsonPropertyName("success")]
         public bool Success { get; set; }
+
         [JsonPropertyName("message")]
         public string Message { get; set; }
+
         [JsonPropertyName("author")]
         public string Author { get; set; }
+
         [JsonPropertyName("date")]
         public string Date { get; set; }
+
+        [JsonPropertyName("roomId")]
+        public string RoomId { get; set; }
     }
 }
